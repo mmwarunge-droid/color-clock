ba1cf936c7ecc77b68467d2216f1b2932a061160
Mark Warunge <mmwarunge@gmail.com>
2026-02-07 10:11:02 +0300

    Display changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,38 @@
-import { useState } from 'react'
 import './App.css'
-import { format } from "date-fns"
+import { useState, useEffect } from "react";
+import { format } from "date-fns";
 
 function App() {
-  const now = new Date ()
+  const [now, setNow] = useState (new Date());
+
+  useEffect (() => {
+    const interval = setInterval (() => {
+      setNow (new Date());
+    }, 1000);
+  
+return () => clearInterval(interval);
+  }, []);
+
+const hours = now.getHours();
+const minutes = now.getMinutes();
+const seconds = now.getSeconds(); 
+
+const red = Math.round((hours / 23) * 255);
+const green = Math.round((minutes / 59) * 255);
+const blue = Math.round((seconds / 59) * 255);
+
+const colorStyle = {
+  backgroundColor: `rgb(${red}, ${green}, ${blue})`,
+};
 
   return (
-    <>
-<div>
-      <p>{format (now, "yyyy-MM-dd HH:mm:ss")}</p>
+  <div className='app'>
+    <div className='clock-card' style={colorStyle}>
+      <p className='clock-time'>
+{format (now, "yyyy-MM-dd HH:mm:ss")}
+</p>
     </div>
-    </>
-  )
+    </div>
+  );
 }
 export default App
